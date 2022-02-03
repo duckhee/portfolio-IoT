04f0519a3ea6c18f2b568ecec1422e8ae29d3dfb
Won <fain9301@yahoo.com>
2022-02-03 19:13:38 +0900

    feat: start paging api resource custom start

--- src/main/java/kr/co/won/user/api/assembler/UserAssembler.java
+++ src/main/java/kr/co/won/user/api/assembler/UserAssembler.java
@@ -5,6 +5,7 @@ import kr.co.won.user.api.resource.dto.UserResourceDto;
 import kr.co.won.user.domain.UserDomain;
 import kr.co.won.user.domain.UserRoleDomain;
 import kr.co.won.user.domain.UserRoleType;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.CollectionModel;
@@ -12,6 +13,8 @@ import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -38,9 +41,33 @@ public class UserAssembler implements RepresentationModelAssembler<UserDomain, U
         return mappedUser;
     }
 
+    public UserResourceDto toModel(UserDomain entity, UserDomain authUser) {
+        UserResourceDto mappedUser = modelMapper.map(entity, UserResourceDto.class);
+        Set<UserRoleType> roles = entity.getRoles().stream().map(UserRoleDomain::getRole).collect(Collectors.toSet());
+        mappedUser.setAddress(entity.getAddress());
+        mappedUser.setActive(entity.isActiveFlag());
+        mappedUser.setDelete(entity.isDeleteFlag());
+        mappedUser.setRoles(roles);
+        WebMvcLinkBuilder selfLink = WebMvcLinkBuilder.linkTo(UserApiController.class);
+        mappedUser.add(selfLink.slash(entity.getIdx()).withSelfRel());
+        if (authUser.hasRole(UserRoleType.ADMIN)) {
+            mappedUser.add(selfLink.slash(entity.getIdx()).withRel("update-users"));
+            mappedUser.add(selfLink.slash(entity.getIdx()).withRel("delete-users"));
+        }
+        return mappedUser;
+    }
+
     @Override
     public CollectionModel<UserResourceDto> toCollectionModel(Iterable<? extends UserDomain> entities) {
+        List<UserResourceDto> list = new ArrayList<>();
+        entities.forEach(entity -> list.add(this.toModel(entity)));
+        return CollectionModel.of(list);
+    }
 
-        return RepresentationModelAssembler.super.toCollectionModel(entities);
+    public CollectionModel<UserResourceDto> toAuthCollection(Iterable<? extends UserDomain> entities, UserDomain authUser) {
+        List<UserResourceDto> list = new ArrayList<>();
+        entities.forEach(entity -> list.add(this.toModel(entity, authUser)));
+        return CollectionModel.of(list);
+
     }
 }
