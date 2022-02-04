9a2364a01d7c60e1e8baa7be1ba86e3a1de7ea78
Won <fain9301@yahoo.com>
2022-02-05 02:04:35 +0900

    feat: add upload file

--- src/main/java/kr/co/won/util/file/FileUtil.java
+++ src/main/java/kr/co/won/util/file/FileUtil.java
@@ -1,7 +1,9 @@
 package kr.co.won.util.file;
 
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 public interface FileUtil {
@@ -10,7 +12,16 @@ public interface FileUtil {
         return;
     }
 
+    public default void fileUpload(ServletFileUpload servletFileUpload) {
+        return;
+    }
+/*
     public default MultipartFile fileDownload(String path) {
         return null;
     }
+
+    public default File fileDownload(String path) {
+        return null;
+    }
+    */
 }
