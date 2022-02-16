78383e4dd37cb18528c44142cf861466d15bd0d4
Won <fain9301@yahoo.com>
2022-02-16 17:54:03 +0900

    feat: add delete function add

--- src/main/java/kr/co/won/blog/controller/BlogController.java
+++ src/main/java/kr/co/won/blog/controller/BlogController.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -81,17 +82,35 @@ public class BlogController {
     }
 
     @GetMapping(path = "/update")
-    public String updateBlogPage(@RequestParam(name = "blog") Long blog, Model model) {
-        return "";
+    public String updateBlogPage(@AuthUser UserDomain loginUser, @RequestParam(name = "blog") Long blogIdx, Model model) {
+        BlogDomain findBlog = blogService.readBlog(blogIdx);
+        List<BlogReplyDomain> findBlogReplies = blogService.listReply(blogIdx);
+        model.addAttribute("blogForm", findBlog);
+        model.addAttribute("replies", findBlogReplies);
+        model.addAttribute("user", loginUser);
+        return "blogs/updateBlogPage";
     }
 
     @PostMapping(path = "/update")
-    public String updateBlogDo(@AuthUser UserDomain loginUser, @RequestParam(name = "blog") Long blog, Model model, RedirectAttributes flash) {
-        return "";
+    public String updateBlogDo(@AuthUser UserDomain loginUser, @RequestParam(name = "blog") Long blogIdx, @Validated BlogForm form, Errors errors, Model model, RedirectAttributes flash) {
+        if (errors.hasErrors()) {
+            List<BlogReplyDomain> findBlogReplies = blogService.listReply(blogIdx);
+            model.addAttribute("replies", findBlogReplies);
+            model.addAttribute("user", loginUser);
+            return "blogs/updateBlogPage";
+        }
+        BlogDomain updateBlog = modelMapper.map(form, BlogDomain.class);
+        blogService.updateBlog(blogIdx, updateBlog, loginUser);
+        return "redirect:/blogs/" + blogIdx;
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteListBlogDo(List<Long> blogIdxes) {
+        return null;
     }
 
     @DeleteMapping(path = "/delete/{blogIdx}")
-    public String deleteBlogDo(@AuthUser UserDomain loginUser, @PathVariable(name = "blogIdx") Long blogIdx, RedirectAttributes flash) {
-        return "";
+    public ResponseEntity deleteBlogDo(@AuthUser UserDomain loginUser, @PathVariable(name = "blogIdx") Long blogIdx, RedirectAttributes flash) {
+        return null;
     }
 }
