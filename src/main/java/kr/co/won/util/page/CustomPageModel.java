05d9b00220e8e5f0ff6a5d49e2057c09e6b4f9a0
Won <fain9301@yahoo.com>
2022-03-08 17:41:45 +0900

    feat: start paging api

--- src/main/java/kr/co/won/util/page/CustomPageModel.java
+++ src/main/java/kr/co/won/util/page/CustomPageModel.java
@@ -1,4 +1,27 @@
 package kr.co.won.util.page;
 
-public class CustomPageModel {
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
+
+import java.util.List;
+
+public class CustomPageModel<T> extends PagedModel<T> {
+
+    @JsonUnwrapped
+    Page page;
+
+    public CustomPageModel(Page<T> page) {
+        long totalElements = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+        Pageable pageable = page.getPageable();
+        int pageSize = pageable.getPageSize();
+        int pageNumber = pageable.getPageNumber();
+        PageMetadata pageMetadata = new PageMetadata(pageSize, pageNumber, totalElements);
+        List<T> content = page.getContent();
+        PagedModel.of(content, pageMetadata);
+    }
+
 }
