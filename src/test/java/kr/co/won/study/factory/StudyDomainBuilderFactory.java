967f8347ce7f126240218b339ea978ba002a366f
Won <fain9301@yahoo.com>
2022-03-08 17:06:07 +0900

    feat: study list test and study find test

--- src/test/java/kr/co/won/study/factory/StudyDomainBuilderFactory.java
+++ src/test/java/kr/co/won/study/factory/StudyDomainBuilderFactory.java
@@ -4,12 +4,15 @@ import kr.co.won.study.domain.StudyDomain;
 import org.springframework.boot.test.context.TestComponent;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 @TestComponent
 public class StudyDomainBuilderFactory {
 
     public StudyDomain makeDefaultStudyInfinityAllowUser(String organizer) {
         StudyDomain mockStudy = StudyDomain.builder()
+                .idx(1L)
                 .name("default study")
                 .allowMemberNumber(0)
                 .description("default study description")
@@ -19,5 +22,37 @@ public class StudyDomainBuilderFactory {
                 .closed(true)
                 .build();
         return mockStudy;
+    }
+
+    public StudyDomain makeDefaultStudyInfinityAllowUser(String organizer, String studyName, String description, String uri) {
+        StudyDomain mockStudy = StudyDomain.builder()
+                .idx(1L)
+                .name(studyName)
+                .allowMemberNumber(0)
+                .description(description)
+                .organizer(organizer)
+                .path(URI.create(uri).toString())
+                .recruiting(false)
+                .closed(true)
+                .build();
+        return mockStudy;
+    }
+
+    public List<StudyDomain> makeMockListStudy(String organizer, int number) {
+        List<StudyDomain> list = new ArrayList<>();
+        for (int i = 0; i < number; i++) {
+            StudyDomain mockStudy = StudyDomain.builder()
+                    .idx(Long.valueOf(i+1))
+                    .name("default study" + i)
+                    .allowMemberNumber(i)
+                    .description("default study description" + i)
+                    .organizer(organizer)
+                    .path(URI.create("/testing/default-study" + i).toString())
+                    .recruiting(false)
+                    .closed(true)
+                    .build();
+            list.add(mockStudy);
+        }
+        return list;
     }
 }
--- src/test/java/kr/co/won/study/service/StudyServiceTest.java
+++ src/test/java/kr/co/won/study/service/StudyServiceTest.java
@@ -1,17 +1,16 @@
 package kr.co.won.study.service;
 
-import kr.co.won.auth.AuthUser;
 import kr.co.won.config.TestAppConfiguration;
 import kr.co.won.study.domain.StudyDomain;
+import kr.co.won.study.factory.StudyDomainBuilderFactory;
 import kr.co.won.study.persistence.StudyPersistence;
 import kr.co.won.user.persistence.UserPersistence;
+import kr.co.won.util.page.PageDto;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
-import org.hibernate.annotations.DiscriminatorFormula;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
@@ -20,20 +19,28 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import javax.annotation.Resource;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
 @Slf4j
-@Import(value = {TestAppConfiguration.class})
+@Import(value = {TestAppConfiguration.class, StudyDomainBuilderFactory.class})
 @ExtendWith(value = {MockitoExtension.class})
 class StudyServiceTest {
 
     private TestAppConfiguration configuration = new TestAppConfiguration();
+
+    private StudyDomainBuilderFactory studyFactory = new StudyDomainBuilderFactory();
 
     @Spy
     @Resource(name = "skipModelMapper")
@@ -55,7 +62,7 @@ class StudyServiceTest {
 
     @DisplayName(value = "01. create study service test - with User")
     @Test
-    void createStudyMockTests() throws Exception {
+    void createStudyMockTests() {
         StudyDomain testStudy = StudyDomain.builder()
                 .allowMemberNumber(0)
                 .name("testStudy")
@@ -69,8 +76,53 @@ class StudyServiceTest {
         assertThat(testStudy.getName()).isEqualTo(study.getName());
     }
 
+    @DisplayName(value = "02. list study service Tests")
+    @Test
+    void listStudyMockTests() {
+        String organizer = "tester@co.kr";
+        int totalNumber = 10;
+        PageDto page = new PageDto();
+        Pageable pageable = page.makePageable(0, "idx");
+        Page listPage = pagingStudy(organizer, totalNumber, pageable);
+        when(studyPersistence.pagingStudy(any(), any(), any())).thenReturn(listPage);
+        Page pagingResult = studyService.pagingStudy(page);
+        // total size setting
+        assertThat(pagingResult.getTotalElements()).isEqualTo(totalNumber);
+        // sort paging test
+        assertThat(pageable.getSort()).isEqualTo(pagingResult.getSort());
+        // paging content
+        assertThat(pagingResult.getContent()).isEqualTo(listPage.getContent());
+
+    }
+
+    @DisplayName(value = "03. find study service Tests")
+    @Test
+    void findStudyWithPathMockTests() {
+        String path = "/test/study";
+        String description = "testingStudy";
+        String studyName = "study";
+        String organizer = "tester@co.kr";
+        // make study
+        StudyDomain studyDomain = studyFactory.makeDefaultStudyInfinityAllowUser(organizer, studyName, description, path);
+        // mock study
+        when(studyPersistence.findByPath(path)).thenReturn(Optional.of(studyDomain));
+        StudyDomain findStudy = studyService.findStudyWithPath(path);
+        // assert
+        assertThat(path).isEqualTo(findStudy.getPath());
+        assertThat(organizer).isEqualTo(findStudy.getOrganizer());
+        assertThat(studyName).isEqualTo(findStudy.getName());
+
+
+    }
+
     private StudyDomain settingIdx(StudyDomain studyDomain) {
         studyDomain.setIdx(1L);
         return studyDomain;
+    }
+
+    // mock paging result
+    private Page pagingStudy(String organizer, int number, Pageable pageable) {
+        List<StudyDomain> mockStudy = studyFactory.makeMockListStudy(organizer, number);
+        return new PageImpl(mockStudy, pageable, 10);
     }
 }
