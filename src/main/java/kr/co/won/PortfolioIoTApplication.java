26c9e7d1a5b45367badce00ef9e843e875a6df81
doukhee <fain9301@yahoo.com>
2022-03-13 23:37:03 +0900

    feat: intergrationComponentScan add

--- src/main/java/kr/co/won/PortfolioIoTApplication.java
+++ src/main/java/kr/co/won/PortfolioIoTApplication.java
@@ -2,7 +2,9 @@ package kr.co.won;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.annotation.IntegrationComponentScan;
 
+//@IntegrationComponentScan
 @SpringBootApplication
 public class PortfolioIoTApplication {
 
