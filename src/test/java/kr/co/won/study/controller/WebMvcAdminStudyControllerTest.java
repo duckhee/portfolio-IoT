30b247d6253a562e7d4cab0d67ef5daaa9863793
doukhee <fain9301@yahoo.com>
2022-03-15 01:46:50 +0900

    need: redirectAttribute mocking test add

--- src/test/java/kr/co/won/study/controller/WebMvcAdminStudyControllerTest.java
+++ src/test/java/kr/co/won/study/controller/WebMvcAdminStudyControllerTest.java
@@ -4,13 +4,20 @@ import kr.co.won.auth.TestMockUser;
 import kr.co.won.config.TestAppConfiguration;
 import kr.co.won.config.TestWebMvcConfig;
 import kr.co.won.config.WebSliceTest;
+import kr.co.won.study.domain.StudyDomain;
+import kr.co.won.study.form.CreateStudyForm;
 import kr.co.won.study.service.StudyService;
 import kr.co.won.study.validation.CreateStudyValidation;
 import kr.co.won.user.domain.UserRoleType;
+import lombok.experimental.FieldDefaults;
 import org.apache.catalina.security.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,11 +28,22 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.intThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -55,6 +73,13 @@ class WebMvcAdminStudyControllerTest {
     @MockBean
     private StudyService studyService;
 
+    @MockBean
+    private RedirectAttributes redirectAttributes;
+
+    @InjectMocks
+    private AdminStudyController adminStudyController;
+
+
     @DisplayName(value = "01. Admin Study Create Page Web Slice Tests")
     @Test
     void studyCreatePageTests() throws Exception {
@@ -73,4 +98,53 @@ class WebMvcAdminStudyControllerTest {
                 .andExpect(model().attributeExists("createStudyForm"))
                 .andExpect(view().name("admin/study/studyCreatePage"));
     }
+
+    @TestMockUser(authLevel = UserRoleType.ADMIN)
+    @DisplayName(value = "02. Admin Study Create Do Web Slice Tests - with ADMIN")
+    @Test
+    void studyCreateDoTests_withADMIN() throws Exception {
+        when(createStudyValidation.supports(any())).thenReturn(true);
+
+        CreateStudyForm testStudy = CreateStudyForm.builder()
+                .name("testStudy")
+                .path("abas")
+                .description("testing study")
+                .shortDescription("short testing")
+                .allowMemberNumber(0)
+                .build();
+        String organizer = "testing@co.kr";
+        StudyDomain study = mockStudy(testStudy);
+        when(studyService.createStudy(study, organizer)).thenReturn(mockSetStudy(study, organizer));
+
+        when(redirectAttributes);
+        mockMvc.perform(post("/admin/study/create")
+                        .with(csrf())
+                        .param("name", testStudy.getName())
+                        .param("path", testStudy.getPath())
+                        .param("description", testStudy.getDescription())
+                        .param("shortDescription", testStudy.getShortDescription())
+                        .param("allowMemberNumber", String.valueOf(testStudy.getAllowMemberNumber()))
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/admin/study/list"));
+    }
+
+
+    private StudyDomain mockStudy(CreateStudyForm form) {
+        return StudyDomain.builder()
+                .name(form.getName())
+                .path(form.getPath())
+                .description(form.getDescription())
+                .shortDescription(form.getShortDescription())
+                .allowMemberNumber(form.getAllowMemberNumber())
+                .build();
+    }
+
+    private StudyDomain mockSetStudy(StudyDomain study, String organizer) {
+        study.setOrganizer(organizer);
+        study.setIdx(1L);
+        return study;
+    }
+
+
 }
