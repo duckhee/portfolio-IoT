86e5865182761d4daf7cb4bc7944e56673ef07cf
doukhee <fain9301@yahoo.com>
2022-04-16 00:03:48 +0900

    Feat: empty iot admin controller

--- src/main/java/kr/co/won/iot/controller/AdminIoTController.java
+++ src/main/java/kr/co/won/iot/controller/AdminIoTController.java
@@ -1,17 +1,81 @@
 package kr.co.won.iot.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.won.auth.AuthUser;
+import kr.co.won.user.domain.UserDomain;
+import kr.co.won.util.page.PageDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
-@RequestMapping(path = "/admin/iot")
+@RequestMapping(path = "/admin/IoT")
 @RequiredArgsConstructor
 public class AdminIoTController {
 
     private final ModelMapper modelMapper;
     private final ObjectMapper objectMapper;
 
+
+    /**
+     * Create IoT Site Create Page
+     */
+    @GetMapping(path = "/site/create")
+    public String iotSiteCreatePage(@AuthUser UserDomain loginUser, Model model) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
+
+    /**
+     * Create IoT Site List Do
+     */
+    @PostMapping(path = "/site/create")
+    public String iotSiteCreateDo(@AuthUser UserDomain loginUser, Model model, RedirectAttributes flash) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
+
+    /**
+     * Create IoT Site List Page
+     */
+    @GetMapping(path = "/site/list")
+    public String iotSiteListPage(@AuthUser UserDomain loginUser, PageDto pageDto, Model model) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
+
+
+    /**
+     * Create IoT Site Create Page
+     */
+    @GetMapping(path = "/device/create")
+    public String iotDeviceCreatePage(@AuthUser UserDomain loginUser, Model model) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
+
+    /**
+     * Create IoT Site List Do
+     */
+    @PostMapping(path = "/device/create")
+    public String iotDeviceCreateDo(@AuthUser UserDomain loginUser, Model model, RedirectAttributes flash) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
+
+    /**
+     * Create IoT Site List Page
+     */
+    @GetMapping(path = "/device/list")
+    public String iotDeviceListPage(@AuthUser UserDomain loginUser, PageDto pageDto, Model model) {
+        log.info("login user information ::: {}", loginUser);
+        return "";
+    }
 }
