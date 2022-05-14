6a0678433fc9a5931af206f0b1954e5a5c2f6d8d
doukhee <fain9301@yahoo.com>
2022-05-14 23:18:31 +0900

    feat: social login start

---
+++ src/main/java/kr/co/won/auth/oauth/OauthUserService.java
@@ -0,0 +1,6 @@
+package kr.co.won.auth.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+
+public class OauthUserService extends DefaultOAuth2UserService {
+}
