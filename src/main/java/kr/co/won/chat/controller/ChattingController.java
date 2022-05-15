4d4ce512e207f1eed535dabc2784706206b70ccb
Won <fain9301@yahoo.com>
2022-05-15 14:47:47 +0900

    fix: profile change local

--- src/main/java/kr/co/won/chat/controller/ChattingController.java
+++ src/main/java/kr/co/won/chat/controller/ChattingController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,19 +20,36 @@ public class ChattingController {
 
     private final ChattingService chattingService;
 
-    @GetMapping
+    @GetMapping(path = "/room")
     public String chattingRoomListPage(@AuthUser UserDomain loginUser, PageDto pageDto, Model model) {
         return "";
     }
 
+    /**
+     * Chatting Room Create Page
+     */
     @GetMapping(path = "/room/create")
-    public String createChattingRoomPage(@AuthUser UserDomain loginUser, Model model){
+    public String createChattingRoomPage(@AuthUser UserDomain loginUser, Model model) {
         return "";
     }
 
+    /**
+     * Chatting Room Create Do
+     */
     @PostMapping(path = "/room/create")
-    public String createChattingRoomDo(@AuthUser UserDomain loginUser, Model model, RedirectAttributes flash){
+    public String createChattingRoomDo(@AuthUser UserDomain loginUser, Model model, RedirectAttributes flash) {
         return "";
+    }
+
+    @GetMapping(path = "/room/update/{roomId}")
+    public String udpateChattingRoomPage(@PathVariable(name = "roomId") Long roomIdx, @AuthUser UserDomain loginUser, Model model) {
+        return "";
+    }
+
+    @PostMapping(path = "/room/update/{roomId}")
+    public String updateChattingRoomDo(@PathVariable(name = "roomId") Long roomIdx, @AuthUser UserDomain loginUser, Model model, RedirectAttributes flash) {
+        return "redirect:/chat/room/";
+
     }
 
 }
