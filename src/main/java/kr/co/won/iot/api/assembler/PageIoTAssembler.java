796d6e03ff93b6adfb3bc0e16ed9bbaf9049d6f0
duckhee <fain9301@yahoo.com>
2022-06-10 16:46:42 +0900

    feat

--- src/main/java/kr/co/won/iot/api/assembler/PageIoTAssembler.java
+++ src/main/java/kr/co/won/iot/api/assembler/PageIoTAssembler.java
@@ -1,4 +1,11 @@
 package kr.co.won.iot.api.assembler;
 
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PageIoTAssembler {
+    private final ModelMapper modelMapper;
 }
